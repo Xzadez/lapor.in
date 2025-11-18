7b1ebcf7f798adfe79f94e81435141ba55bfa908
MaulanaZiddan <77562953+MaulanaZiddan@users.noreply.github.com>
2025-11-18 20:47:35 +0700

    first commit

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,1 +1,39 @@
+import 'package:get/get.dart';
+import 'app_routes.dart';
 
+// Imports Screens
+import '../presentation/login_screen/login_screen.dart';
+import '../presentation/app_navigation_screen/app_navigation_screen.dart';
+
+// Imports Bindings
+import '../presentation/login_screen/binding/login_binding.dart';
+import '../presentation/app_navigation_screen/binding/app_navigation_binding.dart';
+
+class AppPages {
+  // Tentukan rute awal aplikasi (biasanya diambil dari AppRoutes)
+  static const INITIAL = AppRoutes.initialRoute;
+
+  static List<GetPage> pages = [
+    GetPage(
+      name: AppRoutes.loginScreen,
+      page: () => LoginScreen(),
+      bindings: [
+        LoginBinding()
+      ],
+    ),
+    GetPage(
+      name: AppRoutes.appNavigationScreen,
+      page: () => AppNavigationScreen(),
+      bindings: [
+        AppNavigationBinding()
+      ],
+    ),
+    GetPage(
+      name: AppRoutes.initialRoute,
+      page: () => AppNavigationScreen(),
+      bindings: [
+        AppNavigationBinding()
+      ],
+    ),
+  ];
+}

df675eafccf714ea2b57d883ba5b16a53015273d
Rachmat-Mauluddin-043 <mauluddinrachmat@gmail.com>
2025-12-10 09:44:07 +0700

    change name model for home

---
+++ lib/app/presentation/home_screen/model/home_model.dart
@@ -0,0 +1,8 @@
+// Placeholder untuk data laporan
+class BeritaModel {
+  final String title;
+  final DateTime date;
+  final String status;
+
+  BeritaModel({required this.title, required this.date, required this.status});
+}

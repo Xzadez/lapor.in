268dde9fb1c10eccfe4020224710ef774e5984b9
Rachmat-Mauluddin-043 <mauluddinrachmat@gmail.com>
2025-12-12 13:27:42 +0700

    add function logout

--- lib/app/presentation/profile_screen/controller/profile_controller.dart
+++ lib/app/presentation/profile_screen/controller/profile_controller.dart
@@ -1,17 +1,24 @@
 import 'package:get/get.dart';
+import 'package:laporin/app/routes/app_routes.dart';
+import 'package:laporin/app/widgets/custom_snackbar.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class ProfileController extends GetxController {
-  var user = {
-    'name': 'Ahmad Sentosa',
-    'email': 'Ahmadsanzz@gmail.com',
-    'photoUrl': null,
-  }.obs;
+  var user =
+      {
+        'name': 'Ahmad Sentosa',
+        'email': 'Ahmadsanzz@gmail.com',
+        'photoUrl': null,
+      }.obs;
 
-  void logout() {
-    Get.snackbar(
-      'Logout',
-      'Kamu telah keluar.',
-      snackPosition: SnackPosition.BOTTOM,
+  void logout() async {
+    final user = Supabase.instance.client.auth.currentUser;
+    final String username = user?.userMetadata?['username'] ?? '';
+    await Supabase.instance.client.auth.signOut();
+    Get.offAllNamed(AppRoutes.loginScreen);
+    CustomSnackBar.show(
+      message: 'Logout: Kamu telah keluar dari akun $username',
+      isError: false,
     );
   }
 }

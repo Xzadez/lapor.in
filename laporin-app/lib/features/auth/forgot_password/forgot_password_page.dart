09a59981b74487e45f7690cca4b92893a38870ab
MaulanaZiddan <77562953+MaulanaZiddan@users.noreply.github.com>
2025-12-17 06:17:32 +0700

    commit

---
+++ laporin-app/lib/features/auth/forgot_password/forgot_password_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+/// FITUR: Forgot Password
+/// ---------------------------------------------------
+/// Product Owner : Maulana Ziddan
+/// Scrum Master  : -
+/// Developer     : Rachmat Mauluddin, Rafly
+/// ---------------------------------------------------
+
+class ForgotPasswordPage extends StatelessWidget {
+  const ForgotPasswordPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("Reset Password")),
+    );
+  }
+}

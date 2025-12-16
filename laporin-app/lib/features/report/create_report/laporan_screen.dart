698da963dac04e87ae49afddd23582a106f7c64b
MaulanaZiddan <77562953+MaulanaZiddan@users.noreply.github.com>
2025-12-17 06:18:09 +0700

    commit

---
+++ laporin-app/lib/features/report/create_report/laporan_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+/// FITUR: Laporan Screen (Buat Laporan)
+/// ---------------------------------------------------
+/// Product Owner : -
+/// Scrum Master  : Rafly
+/// Developer     : Adriyan
+/// ---------------------------------------------------
+
+class LaporanScreen extends StatelessWidget {
+  const LaporanScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("Form Input Laporan")),
+    );
+  }
+}

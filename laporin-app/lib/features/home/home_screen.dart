af901ec552c3961791cb7ce64118e6240206be21
MaulanaZiddan <77562953+MaulanaZiddan@users.noreply.github.com>
2025-12-17 06:16:48 +0700

    commit

---
+++ laporin-app/lib/features/home/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+/// FITUR: Home Screen
+/// ---------------------------------------------------
+/// Product Owner : -
+/// Scrum Master  : Rachmat Mauluddin
+/// Developer     : Adriyan, Maulana Ziddan
+/// ---------------------------------------------------
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("Dashboard Utama")),
+    );
+  }
+}

e7634cfd5ebdfe514a66bd29bea6144bbd93c1d6
MaulanaZiddan <77562953+MaulanaZiddan@users.noreply.github.com>
2025-12-17 06:15:31 +0700

    commit

---
+++ laporin-app/lib/features/report/detail/detail_laporan_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+/// FITUR: Detail Laporan Page
+/// ---------------------------------------------------
+/// Product Owner : Rachmat Mauluddin
+/// Scrum Master  : Maulana Ziddan, Satria
+/// Developer     : Adriyan
+/// ---------------------------------------------------
+
+class DetailLaporanPage extends StatelessWidget {
+  const DetailLaporanPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("Detail status laporan")),
+    );
+  }
+}

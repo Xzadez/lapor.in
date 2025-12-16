d471362dd4d5eaeac0767509abd25ee4fa1891c0
MaulanaZiddan <77562953+MaulanaZiddan@users.noreply.github.com>
2025-12-17 06:18:44 +0700

    commit

---
+++ laporin-app/lib/features/report/history/riwayat_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+/// FITUR: Riwayat Screen
+/// ---------------------------------------------------
+/// Product Owner : -
+/// Scrum Master  : -
+/// Developer     : Adriyan
+/// ---------------------------------------------------
+
+class RiwayatScreen extends StatelessWidget {
+  const RiwayatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("List Riwayat Laporan")),
+    );
+  }
+}

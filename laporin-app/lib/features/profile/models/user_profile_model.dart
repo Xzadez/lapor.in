15c138ab281cc0a0a0454bb19c51bcbd21eaad35
MaulanaZiddan <77562953+MaulanaZiddan@users.noreply.github.com>
2025-12-17 06:32:33 +0700

    commit

---
+++ laporin-app/lib/features/profile/models/user_profile_model.dart
@@ -0,0 +1,11 @@
+/// MODEL - USER PROFILE
+/// ---------------------------------------------------
+/// Dev Team: Adriyan, Rafly
+/// ---------------------------------------------------
+class UserProfileModel {
+  final String nama;
+  final String email;
+  final String noHp;
+
+  UserProfileModel({required this.nama, required this.email, required this.noHp});
+}

6fcb3fa11ea2376e4b06e796b75e1e7d8c1f9e47
satriaaa7 <129919791+satriaaa7@users.noreply.github.com>
2025-12-26 12:42:59 +0700

    add widget

---
+++ lib/app/widgets/draft_info_box.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class DraftInfoBox extends StatelessWidget {
+  const DraftInfoBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: _CutCornerClipper(),
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+        color: const Color(0xFF7BC9BE),
+        child: const Text(
+          'Form ini akan tersimpan\nsebagai Draft, ketika aplikasi\n'
+          'di tutup/close.',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 13,
+            height: 1.3,
+          ),
+        ),
+      ),
+    );
+  }
+}
